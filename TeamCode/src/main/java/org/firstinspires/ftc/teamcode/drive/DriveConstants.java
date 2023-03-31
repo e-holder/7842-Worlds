fcac31a0ce4da0f9fb7fbe1e8f77df9398069ed3
DrPixelCat24 <joel.a.holder@outlook.com>
2023-03-31 16:16:03 -0500

    Tuned TRACK_WIDTH in DriveConstants.java for turning accuracy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -48,7 +48,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 1.8898; // in
     public static double GEAR_RATIO = 1.00676; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 12; // in
+    public static double TRACK_WIDTH = 16.45; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
