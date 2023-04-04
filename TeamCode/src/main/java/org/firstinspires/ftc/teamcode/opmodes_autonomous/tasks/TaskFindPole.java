ac1f14d54f2a8af66d137906538e6fdcc3b67126
DrPixelCat24 <joel.a.holder@outlook.com>
2023-04-04 16:13:21 -0500

    Ported over some minimal code for converting pole pixel offsets into cone placement drive distances.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes_autonomous/tasks/TaskFindPole.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes_autonomous/tasks/TaskFindPole.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode.opmodes_autonomous.tasks;
 
+import static org.firstinspires.ftc.teamcode.middleware.Vision.NOMINAL_HIGH_POLE_WIDTH_PIX;
+import static org.firstinspires.ftc.teamcode.middleware.Vision.NOMINAL_MID_POLE_WIDTH_PIX;
+
 public class TaskFindPole extends AutonomousTask {
 
     private enum TaskState {
@@ -43,6 +46,61 @@ public class TaskFindPole extends AutonomousTask {
                         " lower " + m_lowerPoleDelta_pix +
                         " width " + m_poleWidth_pix);
         telemetry.update();
+    }
+
+    private final double STRAFE_DIST_PER_PIX_UPPER_HIGH_IN = 1.0 / 60.0; // 1 inch per X pixels
+    private final double STRAFE_DIST_PER_PIX_HIGH_LEAN_IN = 1.0 / 15.0;   // 1 inch per X pixels
+    private final double STRAFE_DIST_HIGH_MAX_IN = 2.5;
+
+    private double getStrafeDistToHighPole_in() {
+        double lean_pix = m_upperPoleDelta_pix - m_lowerPoleDelta_pix;
+        double strafeDist_in = Math.min(STRAFE_DIST_HIGH_MAX_IN,
+                m_upperPoleDelta_pix * STRAFE_DIST_PER_PIX_UPPER_HIGH_IN +
+                        lean_pix * STRAFE_DIST_PER_PIX_HIGH_LEAN_IN);
+        strafeDist_in = Math.max(strafeDist_in, -STRAFE_DIST_HIGH_MAX_IN);
+        return strafeDist_in;
+    }
+
+    private final double STRAFE_DIST_PER_PIX_UPPER_MID_IN = 1.0 / 80.0; // 1 inch per X pixels
+    private final double STRAFE_DIST_PER_PIX_MID_LEAN_IN = 1.0 / 30.0;  // 1 inch per X pixels
+    private final double STRAFE_DIST_MID_MAX_IN = 3.0;
+
+    private double getStrafeDistToMidPole_in() {
+        double lean_pix = m_upperPoleDelta_pix - m_lowerPoleDelta_pix;
+        double strafeDist_in = Math.min(STRAFE_DIST_MID_MAX_IN,
+                m_upperPoleDelta_pix * STRAFE_DIST_PER_PIX_UPPER_MID_IN +
+                        lean_pix * STRAFE_DIST_PER_PIX_MID_LEAN_IN);
+        strafeDist_in = Math.max(strafeDist_in, -STRAFE_DIST_MID_MAX_IN);
+        return strafeDist_in;
+    }
+
+    public double getLateralStrafeToPole_in() {
+        switch (m_poleType) {
+            case HIGH:
+                return getStrafeDistToHighPole_in();
+            case MID:
+            default:
+                return getStrafeDistToMidPole_in();
+        }
+    }
+
+    private final double DIST_PER_PIX_DELTA_WIDTH_HIGH_IN = 1.0 / 10.0; // 1 inch per X pixels
+    private final double DIST_PER_PIX_DELTA_WIDTH_MID_IN = 1.0 / 20.0;  // 1 inch per X pixels
+    private final double PLACE_DIST_MAX_IN = 3.5;
+
+    public double getScoreDistToPole_in() {
+        double scoreDist_in = 0.5 * PLACE_DIST_MAX_IN;
+        switch (m_poleType) {
+            case HIGH:
+                scoreDist_in += ((NOMINAL_HIGH_POLE_WIDTH_PIX - m_poleWidth_pix) *
+                        DIST_PER_PIX_DELTA_WIDTH_HIGH_IN);
+                break;
+            default:
+                scoreDist_in += ((NOMINAL_MID_POLE_WIDTH_PIX - m_poleWidth_pix) *
+                        DIST_PER_PIX_DELTA_WIDTH_MID_IN);
+                break;
+        }
+        return scoreDist_in;
     }
 
     @Override
